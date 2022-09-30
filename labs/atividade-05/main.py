97518f1514cb729ffe8a6f5b9960f322a75eb953
luizcortez18 <luizcortezcasotti@edu.unifil.br>
2022-09-30 11:26:02 -0300

    trocaFigurinha

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,12 +1,33 @@
-def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
-    ''' Implementar a solucao nesta função
-    '''
-    return 0
+def maximizar_troca_de_figurinhas(figurinhaMaria, figurinhaJoao, A, B):
 
+    indm = 0
+    indj = 0 
+    i = 0
+    j = 0
+    m = 0
+    n = 0
 
+    for i in range(int(A)):
+      for j in range(int(B)):
+        if figurinhaMaria[i] == figurinhaJoao[j]:
+          indm += 1
+   
+
+ 
+    for m in range(int(B)):
+      for n in range(int(A)):
+        if figurinhaJoao[m] == figurinhaMaria[n]:
+          indj += 1
+   
+    if (int(A) - int(indm)) < (int(B) - int(indj)):
+        return print(f"O máximo de figurinhas:{int(A) - int(indm)}")
+    else:
+        return print(f"O máximo de figurinhas:{int(B) - int(indj)}")
+ 
+  
 
 if __name__ == '__main__':
     A, B = input().split(' ')
     figurinhas_da_maria = input().split(' ')
     figurinhas_da_joao = input().split(' ')
-    maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao)
+    maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao, A, B)
